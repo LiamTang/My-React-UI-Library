a6e68acdbc536a73110e5f2124bf091495f61c28
liam tang <tangwangjun123@gmail.com>
2020-09-26 20:55:59 +1000

    add componet switch

---
+++ react-ui/src/components/Switch/switch.stories.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from "react";
+import Switch from ".";
+
+export default {
+	title: "UI Component/Input/Switch",
+	component: Switch,
+};
+
+export const Default = () => (
+	<div style={{ padding: "1vw" }}>
+		<Switch />
+	</div>
+);
